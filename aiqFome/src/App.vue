b175b9275193fb3d4fccd1663ad21c6f1ade2e53
almirLimas <104025405+almirLimas@users.noreply.github.com>
2024-03-07 09:33:57 -0300

    configuracoes-init

---
+++ aiqFome/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import NavbarComponent from './components/NavbarComponent.vue'
+</script>
+
+<template>
+  <header>
+    <NavbarComponent />
+
+    <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
